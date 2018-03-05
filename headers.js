f7c85ef5e5937dff96997057ba2d5238ea09dcc5
Daniel_Marcello <daniel.salinas.tellez@gmail.com>
2018-03-05 07:52:53 -0800

    fixes

--- headers.js
+++ headers.js
@@ -1,12 +1,12 @@
 var http = require("http");
 var fs = require("fs");
 
-fs.readFile("./index.html", function(err,html){
-	http.createServer(function(req,res){
+http.createServer(function(req,res){
+	fs.readFile("./index.html", function(err,html){
 		res.writeHead(200,{
 			'Content-Type': 'application/json'
 		})
 		res.write(JSON.stringify({nombre:"Daniel", username:"daniel"}));
 		res.end();
-	}).listen(8080);
-});
+	});
+}).listen(8080);
--- hello_html_async.js
+++ hello_html_async.js
@@ -1,9 +1,9 @@
 var http = require("http");
 var fs = require("fs");
 
-fs.readFile("./index.html", function(err,html){
-	http.createServer(function(req,res){
+http.createServer(function(req,res){
+	fs.readFile("./index.html", function(err,html){
 		res.write(html);
 		res.end();
-	}).listen(8080);
-});
+	});
+}).listen(8080);
